2dc48962b33ea83b7c47b8f968897f66962a13db
Bryan <morganthemosaic@gmail.com>
2024-10-15 18:15:09 -0400

    Setup screen without display

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return <View style={styles.container}><View style={styles.search}><TextInput placeholder="Who are you looking for?"
+    placeholderTextColor="#7D7D7D"
+    selectionColor="#1DB954" style={styles.textInput} /></View><View style={styles.display}></View></View>
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { backgroundColor: "#121212", flex: 1 }, display: { flex: 4 }, search: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 8,
+    marginTop: 64,
+    width: "100%"
+  }, textInput: {
+    borderColor: "#1DB954",
+    borderRadius: 5,
+    borderWidth: 1,
+    color: "#FCFCFC",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    height: 48,
+    marginHorizontal: 16,
+    paddingHorizontal: 8
+  }
+})
+
+export default App;
